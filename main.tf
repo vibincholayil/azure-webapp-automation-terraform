2637aaa121bcb179869dbf2107588646fe56c7a4
vibincholayil <vibinnath.c@gmail.com>
2025-07-13 19:06:40 +0100

    update

--- main.tf
+++ main.tf
@@ -1,76 +1,31 @@
-/*
-# Specify the provider
-provider "azurerm" {
-  features {}
-  subscription_id = "81697b3e-f6a5-4d7f-8a2b-59a42bb0b8e9"
-}
 
-# Define the Azure Resource Group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-# Define the Azure App Service Plan (linux - Free Tier)
-resource "azurerm_service_plan" "example" {
-  name                = "example-app-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  os_type   = "linux"
-  sku_name  = "F1"  
-}
-
-# Define the Azure App Service (Web App)
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_service_plan.example.id
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-}
-
-
-*/
 
 provider "azurerm" {
   features {}
+  use_cli = true
   subscription_id = "81697b3e-f6a5-4d7f-8a2b-59a42bb0b8e9"
-  
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo-flask"
+  location = "West Europe" # or another region with F1 quota
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+resource "azurerm_service_plan" "asp" {
+  name                = "demo-flask-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "flask-helloworld-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
+    always_on = false
   }
 }
-
